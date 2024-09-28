b70ee5c71f3221cf8a6871977ae63a011e7aa3eb
TimChen1011 <110208059@g.nccu.edu.tw>
2024-09-28 21:28:40 +0800

    feat: Add callback.js for asynchronous job execution

---
+++ week-03/callback.js
@@ -0,0 +1,42 @@
+//doJob 函式不能修改
+function doJob(job, time, cb) {
+  setTimeout(() => {
+    // 只有在這裡，才能知道這個非同步的 setTimeout 已經做完事情了
+    let now = new Date();
+    cb(`完成工作 ${job} at ${now.toISOString()}`);
+  }, time);
+}
+
+// 刷牙 1sec -> 吃早餐 3 sec -> 寫功課 1sec -> 吃午餐 2sec
+let now = new Date();
+console.log(`開始工作 at ${now.toISOString()}`);
+// write your code here
+// 以下是使用範例
+// call+back => 做完我再叫你的函式，可以被放進函式參數放進去其他參數
+// 原本的痛點  使用 setTimeout非同步 但是動作順序不是我想要的。
+// 我有很多個doJob 但是我想要他們按照我想要的順序執行 => 使用 callback
+
+doJob("刷牙", 1000, function (data) {
+  console.log(data);
+  doJob("吃早餐", 3000, function (data) {
+    console.log(data);
+    doJob("寫功課", 1000, function (data) {
+      console.log(data);
+      doJob("吃午餐", 2000, function (data) {
+        console.log(data);
+      });
+    });
+  });
+});
+// 發現有一個問題，如果doJob越來越多，會有很多層的callback，這樣的寫法會造成callback hell
+// 我們可以使用 promise 來解決這個問題
+
+/*
+開始工作的時間是 19:07:20，1 秒後完成刷牙，再 3 秒後完成吃早餐，再 1 秒後完成寫功課，再 2 秒後完成吃午餐
+期望輸出
+開始工作 at 2024-09-25T19:07:20.167Z
+完成工作 刷牙 at 2024-09-25T19:07:21.196Z
+完成工作 吃早餐 at 2024-09-25T19:07:24.198Z
+完成工作 寫功課 at 2024-09-25T19:07:25.199Z
+完成工作 吃午餐 at 2024-09-25T19:07:27.199Z
+*/
---
+++ week-03/readme.md
@@ -0,0 +1,17 @@
+註冊 AWS 帳號
+
+- 什麼是 AWS Region, AZ (availability zones)
+在 AWS Regions 選單可以看到有不同地區可以選，AWS 服務部署在全球各地的不同地理區域，每個區域都有自己的基礎設施和服務，
+來提供不同地區的使用者使用 AWS 的服務。 AZ (availability zones) 是位於同一區域內的多個獨立資料中心。每個 AZ 都有獨立設備，並透過低延遲連接彼此。
+好處是可以防止單點故障影響整個區域，當某一點壞掉可以替換成另外一個，提高容錯性。 
+
+ 
+- 如果你要使用 AWS 服務，你會怎麼選擇用哪個 Region，考慮的因素有哪些？
+
+我分別會考量價格、速度。
+我會選擇用 Asia Pacific (Malaysia) ， 首先根據馬拉西亞的平均價格是較低，馬拉西亞離臺灣地理位置較近，延遲比較低。
+
+
+
+#### 參考
+-[Average Price for AWS Regions](https://cloudprice.net/aws/regions?sortField=__Geography&sortOrder=true)
